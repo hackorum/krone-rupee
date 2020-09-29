c23dbea4850704b57c57ef9bc0fde4df37735923
hackorum <neilsabde@gmsil.com>
2020-09-29 15:43:24 +0530

    Added Files

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var input, button, sentence;
+
+function setup() {
+   createCanvas(710, 400);
+
+   input = createInput();
+   input.position(20, 65);
+
+   button = createButton('convert');
+   button.position(input.x + input.width, 65);
+   button.mousePressed(KroneInRupee);
+
+   sentence = createElement('h2', 'Enter amount in Danish Krones:');
+   sentence.position(20, 5);
+
+   textAlign(CENTER);
+   textSize(50);
+}
+
+async function KroneInRupee() {
+  const krone = input.value();
+  var response = await fetch("https://api.exchangeratesapi.io/latest?base=DKK");
+  var responseJSON = await response.json();
+  var rates = responseJSON.rates;
+  var inr = rates.INR;
+
+  sentence.html('Amount in ₹upees: ' + krone*inr);
+  input.value('');
+
+}
